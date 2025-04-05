928494cb792e2f1812834b7f85d653278f9179eb
Amar Shaw <67730823+AmarKumarShaw@users.noreply.github.com>
2025-04-05 12:31:33 +0530

    Prod Build....

--- src/lib/sanityClient.ts
+++ src/lib/sanityClient.ts
@@ -12,7 +12,7 @@
 import { createClient } from "@sanity/client";
 
 export const sanityClient = createClient({
-  projectId: import.meta.env.VITE_SANITY_PROJECT_ID || "sr758iyp", // ✅ pulled from .env
+  projectId: import.meta.env.VITE_SANITY_PROJECT_ID, // ✅ pulled from .env
   dataset: import.meta.env.VITE_SANITY_DATASET || "production",      // ✅ pulled from .env
   useCdn: true,                                       // faster queries in production
   apiVersion: "2024-01-01",                           // set to a specific date
